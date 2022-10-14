df3d086d81f4d1cf69c3a1ce37408fd64d9d0211
Audun Stien <audun97@gmail.com>
2022-10-14 21:59:40 +0000

    refactor: fix casing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { FetchAndDisplayImage } from './Fetchimage';
+import { FetchAndDisplayImage } from './FetchImage';
 import { useEffect, useState } from 'react';
 import React from 'react';
 import ReactDOM from 'react-dom'
