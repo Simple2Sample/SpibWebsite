b74e06b8987027dd683aa9208c049b30d5af2122
Audun Stien <audun97@gmail.com>
2022-10-14 22:14:49 +0000

    fix formatting

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,11 @@ import './App.css';
 import { FetchAndDisplayImage } from './FetchImage';
 import React from 'react';
 
-
 function App() {
   return (
-
     <div className="App" title='Spib'>
-      <div><FetchAndDisplayImage></FetchAndDisplayImage></div>
+      <FetchAndDisplayImage />
     </div>
-   
   );
 }
 
--- src/FetchImage.jsx
+++ src/FetchImage.jsx
@@ -1,31 +1,27 @@
 import { useState, useEffect } from "react";
 
+const apiURL = "https://derpibooru.org/api/v1/json/search/images?q=safe,-pregnancy,-diaper,-artist:mykegreywolf,spitfire,solo,score.gte%3A150&sf=random";
+
 export const FetchAndDisplayImage = () => {
-  let [data,setData] = useState([]);
-  let [ID,setID] = useState([]);
+  const [data, setData] = useState("");
+  const [ID, setID] = useState("");
 
+  const openInNewTab = url => {
+    window.open(url, '_blank', 'noopener,noreferrer');
+  }
 
+  useEffect(() => {
+    fetch(apiURL)
+    .then((res) =>
+      res.json())
+    .then((response) => {
+      console.log(response);
+      setData(response.images[0].view_url);
+      setID(response.images[0].id);
+    })
+  }, [])
 
-    const apiURL = "https://derpibooru.org/api/v1/json/search/images?q=safe,-pregnancy,-diaper,-artist:mykegreywolf,spitfire,-screencap,-comic,-irl,score.gte%3A150&sf=random";
-    const openInNewTab = url => {
-      window.open(url, '_blank', 'noopener,noreferrer');}
-useEffect(() => {
-
-      fetch(apiURL)
-        .then((res) =>
-          res.json())
-   
-        .then((response) => {
-          console.log(response);
-          setData(response.images[0].view_url);
-          setID(response.images[0].id);
-        })},[])
-              
-                if(data !== []){
-                return (<div><img src={data} height={window.innerHeight} onClick={() => openInNewTab("https://derpibooru.org/images/"+ID)} /></div>)
-                }
-              }
-        
-    
-
-  
+  if(data !== ""){
+    return (<div><img src={data} height={window.innerHeight} onClick={() => openInNewTab("https://derpibooru.org/images/"+ID)} /></div>)
+  }
+}
