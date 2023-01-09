ce5b7a85f115ca73b629ab72d8813c2126531e1b
Simon <simon.saetre@gmail.com>
2023-01-09 21:52:59 +0100

    Tons of changes

--- src/FetchImage.jsx
+++ src/FetchImage.jsx
@@ -1,23 +1,58 @@
 import { useState, useEffect } from "react";
-
-const apiURL = "https://derpibooru.org/api/v1/json/search/images?q=safe,-pregnancy,-diaper,-artist:mykegreywolf,spitfire,-screencap,-comic,-original_format:mp4,-irl,score.gte%3A150&sf=random";
+import { useSwipeable } from "react-swipeable";
 
 export const FetchAndDisplayImage = () => {
-  const [images, setImages] = useState([])
-  const [currImgNo, setCurrImgNo] = useState(0)
+  const [images, setImages] = useState([]);
+  const [currImgNo, setCurrImgNo] = useState(0);
+  const [apiURL, setApiURL] = useState('');
+  const [loadingText, setLoadingText] = useState("Loading yellow fire horse...");
 
   const openInNewTab = () => {
     window.open("https://derpibooru.org/images/" + images[currImgNo].id, '_blank', 'noopener,noreferrer');
   }
 
-  useEffect(() => {
-    getImages()
+
+
+  const swipeHandlers = useSwipeable({
+    onSwipedLeft: () => nextImage(),
+    onSwipedRight: () => prevImage(),
+    onTap: () => openInNewTab(),
+    trackMouse: true,
+    swipeDuration: 500,
+    delta: 5
+  });
+
+const checkQuery = () => {
+  if ( window.location.search === "?nsfw") {
+
+    setLoadingText("Loading lewd yellow fire horse...");
+    setApiURL("https://derpibooru.org/api/v1/json/search/images?filter_id=195915&q=explicit,-diaper,-foalcon,-artist:mykegreywolf,-sfm,spitfire,-screencap,-comic,-original_format:mp4,-original_format:webm,-irl,score.gte%3A250&sf=random");
+return;
+  }
+  setApiURL("https://derpibooru.org/api/v1/json/search/images?q=safe,-pregnancy,-diaper,-artist:mykegreywolf,spitfire,-screencap,-comic,-original_format:mp4,-original_format:webm,-irl,score.gte%3A150&sf=random");
+  return;
+}
+
+useEffect(() => {
+  checkQuery();
+
+
   }, [])
 
+  useEffect(() => {
+    if (apiURL)
+    {
+    getImages();
+    }
+  }, [apiURL])
+
   const nextImage = () => {
-    if (currImgNo === images.length - 1) {
-      getImages()
-      setCurrImgNo(0)
+    // if one more remains
+
+    if (currImgNo == images.length - 14 ) {
+
+       getImages();
+       setCurrImgNo(currImgNo + 1)
     } else {
       setCurrImgNo(currImgNo + 1)
     }
@@ -30,15 +65,20 @@ export const FetchAndDisplayImage = () => {
   }
 
   const getImages = () => {
+
     fetch(apiURL)
     .then((res) => res.json())
     .then((response) => {
       console.log(response);
-      setImages(response.images)
+      const newList = [...images, ...response.images];
+      setImages(newList);
     })
   }
 
   const handleKeyDown = e => {
+    if (e.repeat) {
+      return;
+    }
     switch (e.key) {
       case "l":
       case "d":
@@ -54,12 +94,14 @@ export const FetchAndDisplayImage = () => {
   }
 
   if(images.length === 0){
-    return <p>Loading...</p>
+    return( <p>{loadingText}</p>
+    
+    )
   }
   return (
-    <div style={{display:"flex",alignItems:"center",justifyContent:"space-around", background:"black", width: "100vw", height: "100vh"}} tabIndex="0" onKeyDown={handleKeyDown} >
+    <div  style={{display:"flex",alignItems:"center",justifyContent:"space-around", background:"black", width: "100vw", height: "100vh"}} tabIndex="0" onKeyDown={handleKeyDown} >
 
-      <img alt="" src={images[currImgNo].view_url} style={{background:"white",cursor: "pointer",maxHeight:"100%",height:"auto",maxWidth:"100%"}} onClick={() => openInNewTab()} />
+      <img alt="Image of a beautiful pegaus called Spitfire" {...swipeHandlers} src={images[currImgNo].view_url} style={{background:"white",cursor: "pointer",maxHeight:"100%",height:"auto",maxWidth:"100%"}} draggable={false}  />
     </div>
 
   )
