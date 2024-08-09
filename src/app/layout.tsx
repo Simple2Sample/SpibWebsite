91516b6250825128fa3517a38da2663ad3caca44
Simon <simon.saetre@gmail.com>
2024-08-09 19:22:30 +0200

    adding styled

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import styled, { ThemeProvider } from "styled-components";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,11 +21,18 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const theme = {
+    main: "mediumseagreen"
+  };
+
   return (
     
     <html lang="en">
-<img src="/SpibBounce.gif" />
+
+
       <body className={inter.className}>{children}</body>
+
     </html>
   );
 }
---
+++ src/components/styles/mainBar.Styles.tsx
@@ -0,0 +1,10 @@
+import { styled } from "styled-components";
+
+
+const MainPageGrid = styled.div`
+
+grid-template-rows: 64px 1fr 48px;
+grid-template-columns: 1fr;
+grid-column-gap: 16px;
+`
+
