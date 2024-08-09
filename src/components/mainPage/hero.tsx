c253ecc62ef0d998fcbdcd1536174e64e33ec441
Simon <simon.saetre@gmail.com>
2024-08-09 22:56:09 +0200

    Text changes

--- src/components/mainPage/hero.tsx
+++ src/components/mainPage/hero.tsx
@@ -13,10 +13,10 @@ const Hero = () => {
     <StyledHero className="px-4 py-5 my-5 text-center">
     <h1 className="display-5 fw-bold">Spib = Spitfire</h1>
     <div className="col-lg-6 mx-auto">
-      <p className="lead mb-4">{`No one truly knows the origin of the nickname "Spib". But I guess it's easier to say.`}</p>
+      <p className="lead mb-4">{`No one truly knows the origin of the nickname "Spib", but it's easier to say.`}</p>
       <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-        <button type="button" className="btn btn-primary btn-lg px-4 gap-3">Primary button</button>
-        <button type="button" className="btn btn-outline-secondary btn-lg px-4">Secondary</button>
+        <button type="button" className="btn btn-primary btn-lg px-4 gap-3">I want Spib pics</button>
+        <button type="button" className="btn btn-outline-secondary btn-lg px-4">Who is Spitfire?</button>
       </div>
     </div>
   </StyledHero>
