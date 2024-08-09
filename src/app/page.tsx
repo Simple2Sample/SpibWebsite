9fd9d733b5b861821c64e49e26900f793e1e7cbc
Simon <simon.saetre@gmail.com>
2024-08-09 20:15:06 +0200

    navbar done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,66 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { styled } from "styled-components";
+import { MainPageGrid } from "@/components/styles/mainBar.Styles";
+import Logo192 from "@/public/logo192.png";
 
 const Test = styled.img`
 
-width:50%;
-height:10%;
+width:100%;
+height:100%;
+grid-column: 1 / -1;
+grid-row: 2 / -2;
 
-`
+
+`;
+
+
+const Logo = styled.img`
+margin-bottom: 10px;
+margin-right: 16px;
+margin-left: 8px;
+`;
+
 export default function Home() {
 
 
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-      
+ 
+      <MainPageGrid >
+
+   
+
+<nav className="navbar navbar-expand-lg navbar-light bg-light">
+<a href="/"><Logo src="/logo192.png" width="40"  height="40" className="d-inline-block align-top " alt="" /></a>
+  <a className="navbar-brand" href="#">Spib.no</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarNav">
+    <ul className="navbar-nav">
+      <li className="nav-item active">
+        <a className="nav-link fw-bold" href="#">Home <span className="sr-only"></span></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Infinite Spitfire</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">About Me</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Contact</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+  </div>
+</nav>
       
         <Test src="/SpibBounce.gif" />
-      </div>
+        </MainPageGrid>
+   
     </main>
   );
 }
--- src/components/styles/mainBar.Styles.tsx
+++ src/components/styles/mainBar.Styles.tsx
@@ -2,9 +2,11 @@ import { styled } from "styled-components";
 
 
 export const MainPageGrid = styled.div`
-
+display: grid;
 grid-template-rows: 64px 1fr 48px;
 grid-template-columns: 1fr;
 grid-column-gap: 16px;
-`
+height: 100dvh;
+width: 100dvw;
+`;
 
