5cf7f98bac0b31aaa6f32b3ebe2a5f59c52d9fc0
Simon <simon.saetre@gmail.com>
2024-08-11 16:42:42 +0200

    Removed scroll bar

--- src/components/styles/mainBar.Styles.tsx
+++ src/components/styles/mainBar.Styles.tsx
@@ -3,7 +3,7 @@ import { styled } from "styled-components";
 
 export const MainPageGrid = styled.div`
 display: grid;
-grid-template-rows: 64px 400px 32px 1fr 48px;
+grid-template-rows: 64px 390px 32px 1fr 48px;
 grid-template-columns: 1fr 1fr;
 height: 100dvh;
 width: 100dvw;
