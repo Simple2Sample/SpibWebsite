7218fbe2b4578cbbfe4b240a9eb0388d2c8f117e
Simon <simon.saetre@gmail.com>
2024-08-11 20:20:48 +0200

    Testing removing basepath

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    basePath: "/SpibWebsite",
+   
         compiler: {
           // Enables the styled-components SWC transform
           styledComponents: true
