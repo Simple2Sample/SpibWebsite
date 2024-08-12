49f617ee8741f06643166ecc34582158b8fae093
Simon <simon.saetre@gmail.com>
2024-08-12 19:44:49 +0200

    Revert "Fixed open in new tab being shitty"
    This reverts commit 2c6e1a0c79f94d1da295cfb0978b409139f84449.

--- src/app/infiniteSpitfire/page.tsx
+++ src/app/infiniteSpitfire/page.tsx
@@ -73,6 +73,7 @@ const StyledInstructions = styled.p`
   const swipeHandlers = useSwipeable({
     onSwipedLeft: () => nextImage(),
     onSwipedRight: () => prevImage(),
+    onTap: () => openInNewTab(),
     trackMouse: true,
     swipeDuration: 500,
     delta: 5
@@ -194,7 +195,7 @@ if (images[currImgNo+1]) {
       {!images.length ? (<LoadingContainer><h1>{loadingText}</h1></LoadingContainer>) : (
 
 
-     <StyledImage alt="Image of a beautiful pegaus called Spitfire" {...swipeHandlers} src={images[currImgNo].representations.large} onClick={() =>openInNewTab()} draggable={false}  />
+      <StyledImage alt="Image of a beautiful pegaus called Spitfire" {...swipeHandlers} src={images[currImgNo].representations.large} draggable={false}  />
       )}
     </ContentContainer>
     <InstructionsContainer>
