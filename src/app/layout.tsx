336b6fa11e1c7445fad0352d2a99f78e2c7f414b
Simon <simon.saetre@gmail.com>
2024-08-26 18:45:01 +0200

    Made layout SSR

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,44 +1,19 @@
-"use client";
+
 import 'bootstrap/dist/css/bootstrap.css';
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Head from 'next/head';
-import { useEffect, useState } from 'react';
-import { styled } from 'styled-components';
+import Providers from '@/components/providers';
 
 
 const inter = Inter({ subsets: ["latin"] });
-const LoadingContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  width: 100vw;
-`;
-const LoadingText = styled.p`
-  text-align: center;
-`;
-
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    const handleLoad = () => {
-      setIsLoading(false);
-    };
-
-    if (document.readyState === 'complete') {
-      handleLoad();
-    } else {
-      window.addEventListener('load', handleLoad);
-      return () => window.removeEventListener('load', handleLoad);
-    }
-  }, []);
 
   return (
     <html lang="en">
@@ -72,14 +47,10 @@ export default function RootLayout({
           }
         `}</style>
       </Head>
-      <body className={`${inter.className} ${isLoading ? '' : 'loaded'}`}>
-        {isLoading ? (
-          <LoadingContainer>
-            <LoadingText>Loading...</LoadingText>
-          </LoadingContainer>
-        ) : (
-          children
-        )}
+      <body className={inter.className}>
+        <Providers>
+          {children}
+          </Providers>
       </body>
     </html>
   );
