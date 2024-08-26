0a7249aa74e13cb8112d7083e52642bc46d8ef04
Simon <simon.saetre@gmail.com>
2024-08-26 20:18:51 +0200

    Trying stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   description : "Spitfire = Spib",
+  colorScheme:'dark',
   title: "Spib",
   applicationName: 'Spib.no',
   creator: 'Simple Sæmple',
@@ -19,12 +20,13 @@ export const metadata: Metadata = {
     description: 'Spitfire = Spib',
     type: 'website',
     siteName: 'Spib.no',
-    images: [
+    images: 
       {
         url: 'https://derpicdn.net/img/view/2015/5/14/896071.gif',
         alt: 'The pegasus Spitfire',
+        width:150
       },
-    ],
+    
   },
 }
 
